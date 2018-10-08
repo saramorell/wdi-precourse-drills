384ff2a4a8ba20314a27897d3fa6e2e14998b50a
saramorell <saramorell@gmail.com>
2018-10-07 21:32:06 -0600

    Add files via upload

---
+++ wdi_precourse_website/scripts.js
@@ -0,0 +1,22 @@
+var body = document.getElementsByTagName("body")[0]
+var header = document.getElementById("head")
+
+function thistle() {
+    document.getElementById("head")
+    head.style.backgroundColor = "Thistle"
+}
+
+function olive() {
+    document.getElementById("head")
+    head.style.backgroundColor = "Olive"
+}
+
+function indianRed() {
+    document.getElementById("head")
+    head.style.backgroundColor = "IndianRed"
+}
+
+function cadetBlue() {
+    document.getElementById("head")
+    head.style.backgroundColor = "CadetBlue"
+}
